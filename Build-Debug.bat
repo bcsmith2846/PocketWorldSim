76bca1931354f4431fd294291ebab3a9ec2539bd
Brenden Smith <bcsmith2846@gmail.com>
2020-07-01 22:38:58 -0500

    Creating a build/publish script

---
+++ Build-Debug.bat
@@ -0,0 +1,17 @@
+﻿ECHO OFF
+CLS
+if NOT [%2] == [] GOTO CONFIG
+
+if not exist %~dp0\bin\%1 (mkdir %~dp0\bin\%1)
+if not exist %~dp0\bin\%1\debug (mkdir %~dp0\bin\%1\debug)
+dotnet publish -o %~dp0\bin\%1\debug -r %1 --self-contained false -c debug
+GOTO END
+
+:CONFIG
+if not exist %~dp0\bin\%1 (mkdir %~dp0\bin\%1)
+if not exist %~dp0\bin\%1\%2 (mkdir %~dp0\bin\%1\%2)
+dotnet publish -o %~dp0\bin\%1\%2 -r %1 --self-contained false -c %2
+GOTO END
+
+:END
+goto:eof
