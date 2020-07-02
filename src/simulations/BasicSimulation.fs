aee241d414059c1f541ff925d2ddbac80878c29e
Brenden Smith <bcsmith2846@gmail.com>
2020-07-01 21:37:52 -0500

    Adding more types and modules
    - Added the Person type
    - Added event types
    - Added basic simulating

---
+++ src/simulations/BasicSimulation.fs
@@ -0,0 +1,17 @@
+﻿namespace PocketWorldSim.Simulations
+
+module BasicSimulation =
+    open System
+
+    let RandomNormal (rand: Random) mean stdDev = 
+        let u1 = 1.0 - rand.NextDouble()
+        let u2 = 1.0 - rand.NextDouble()
+        let randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                            Math.Sin(2.0 * Math.PI * u2)
+        mean + stdDev * randStdNormal
+
+    let QuickSim person events =
+        let rand = new Random()
+        let work_arrival = (RandomNormal rand 20.0 10.0) - 10.0
+        printf "%A" work_arrival
+
